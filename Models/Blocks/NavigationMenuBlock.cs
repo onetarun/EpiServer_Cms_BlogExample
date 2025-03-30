d40dd019a5e805cf0c4a50da52bd79ae765598cf
Tarun Kumar <onetarun@outlook.com>
2025-03-30 18:47:38 +0530

    Delete Default Created Folder

---
+++ Models/Blocks/NavigationMenuBlock.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.SpecializedProperties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part1.Models.Blocks
+{
+    [ContentType(DisplayName = "Navigation Menu Block", GUID = "edc148ed-9d42-4b8b-b4e7-bd857fa4a345", Description = "Menu block for top navigation")]
+    public class NavigationMenuBlock : BlockData
+    {
+        [Display(Name = "Menu Items", Order = 10)]
+        public virtual LinkItemCollection MenuItems { get; set; }
+    }
+}
